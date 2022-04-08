db0ebe4f48c08c34c2124c32ca75b4eae8bb30c1
Yamauchi <yamauchi.yy@om.asahi-kasei.co.jp>
2022-04-08 11:24:55 +0900

    create card

--- src/types.ts
+++ src/types.ts
@@ -2,15 +2,17 @@
 
 export interface WeeklyCalendarCardConfig {
   type: string;
-  //  entity: string;
+  entity: string;
   show_last_weeks: number;
   show_follow_weeks: number;
   start_weekday: number;
   today_background_color: string;
+  today_text_color: string;
   weekday_background_color: WeekdayBackgroundColorConfig[];
 }
 
 export interface WeekdayBackgroundColorConfig {
   weekday: number;
   background_color: string;
+  text_color: string;
 }
--- src/weekly-calendar-card.ts
+++ src/weekly-calendar-card.ts
@@ -1,153 +1,169 @@
-import { LitElement, property, customElement, css, CSSResult, html, TemplateResult } from 'lit-element';
-import { HomeAssistant, LovelaceCard, createThing, LovelaceCardConfig } from 'custom-card-helpers';
+import {
+  LitElement,
+  html,
+  customElement,
+  property,
+  TemplateResult,
+  css,
+  CSSResult,
+  PropertyValues,
+  unsafeCSS,
+} from 'lit-element';
+import { HomeAssistant } from 'custom-card-helpers';
+
 import { WeeklyCalendarCardConfig } from './types';
 
 @customElement('weekly-calendar-card')
-class WeeklyCalendarCard extends LitElement implements LovelaceCard {
-  protected _card?: LovelaceCard;
-  private _config?: WeeklyCalendarCardConfig;
-
-  private _hass?: HomeAssistant;
-  private _cardPromise: Promise<LovelaceCard> | undefined;
-
-  set hass(hass: HomeAssistant) {
-    this._hass = hass;
-    if (this._card) {
-      this._card.hass = hass;
-    }
-  }
-
-  static get styles(): CSSResult {
-    return css`
-      ha-card {
-        overflow: hidden;
-      }
-    `;
-  }
+class WeeklyCalendarCard extends LitElement {
+  @property() public hass?: HomeAssistant;
+  @property() private _config?: WeeklyCalendarCardConfig;
 
   public setConfig(config: WeeklyCalendarCardConfig): void {
-    if (!config) {
+    if (!config.entity) {
       throw new Error(`There is no cards parameter defined`);
     }
-    this._config = {
-      // entity: [],
-      type: '',
-      show_last_weeks: 1,
-      show_follow_weeks: 2,
-      start_weekday: 0,
-      today_background_color: '#ff0000',
-      weekday_background_color: [
-        {
-          //Sun
-          weekday: 0,
-          background_color: '#ff0000',
-        },
-        {
-          //Sat
-          weekday: 6,
-          background_color: '#ff0000',
-        },
-      ],
-    };
-    this._createStack();
+    config.show_last_weeks = !config.show_last_weeks ? 1 : config.show_last_weeks;
+    config.show_follow_weeks = !config.show_follow_weeks ? 2 : config.show_follow_weeks;
+    config.start_weekday = !config.start_weekday ? 0 : config.start_weekday;
+    config.today_background_color = !config.today_background_color ? '#ff0000' : config.today_background_color;
+    config.today_text_color = !config.today_text_color ? '#000000' : config.today_text_color;
+    config.weekday_background_color = !config.weekday_background_color
+      ? [
+          {
+            //Sun
+            weekday: 0,
+            background_color: '#ff0000',
+            text_color: '#000000',
+          },
+          {
+            //Sat
+            weekday: 6,
+            background_color: '#0000ff',
+            text_color: '#000000',
+          },
+        ]
+      : config.weekday_background_color;
+    this._config = config;
   }
 
-  private async _createStack() {
-    this._cardPromise = this._createCard({
-      type: `weekly-calendar-card`,
-    });
-    this._card = await this._cardPromise;
-  }
-
-  protected render(): TemplateResult {
-    if (!this._hass || !this._config) {
-      return html``;
-    }
-
-    return html`
-      <ha-card header="weekly-calendar-card">
-        <div>${this._card}</div>
-      </ha-card>
-    `;
-  }
-  public async getCardSize(): Promise<number> {
-    await this._cardPromise;
-    if (!this._card) {
-      return 0;
-    }
+  public getCardSize(): number {
     return 1;
   }
 
-  private async _createCard(config: LovelaceCardConfig): Promise<LovelaceCard> {
-    const element: LovelaceCard = createThing(config);
-    // if (HELPERS) {
-    //   element = (await HELPERS).createCardElement(config);
-    // } else {
-    //   element = createThing(config);
-    // }
-    // if (this._hass) {
-    //   element.hass = this._hass;
-    // }
-    // if (element) {
-    //   element.addEventListener(
-    //     "ll-rebuild",
-    //     (ev) => {
-    //       ev.stopPropagation();
-    //       this._rebuildCard(element, config);
-    //     },
-    //     { once: true }
-    //   );
-    // }
+  protected shouldUpdate(changedProps: PropertyValues): boolean {
+    if (changedProps.has('_config')) {
+      return true;
+    }
+
+    if (this.hass && this._config) {
+      const oldHass = changedProps.get('hass') as HomeAssistant | undefined;
+
+      if (oldHass) {
+        return oldHass.states[this._config.entity] !== this.hass.states[this._config.entity];
+      }
+    }
+
+    return true;
+  }
+
+  protected render(): TemplateResult | void {
+    if (!this._config || !this.hass) {
+      return html``;
+    }
+    const stateObj = this.hass.states[this._config.entity];
+    if (!stateObj) {
+      return html`
+        <ha-card>
+          <div class="warning">
+            Entity not available: ${this._config.entity}
+          </div>
+        </ha-card>
+      `;
+    }
+
     const today = new Date();
     const startDay = new Date(
-      today.setDate(
-        today.getDate() - today.getDay() + (this._config!.start_weekday - 7 * this._config!.show_last_weeks),
-      ),
+      today.getTime() -
+        (today.getDay() -
+          this._config.start_weekday +
+          (today.getDay() >= this._config.start_weekday ? 0 : 7) +
+          7 * this._config.show_last_weeks) *
+          24 *
+          60 *
+          60 *
+          1000,
     );
     const endDay = new Date(
-      startDay.getDate() + 7 * (this._config!.show_last_weeks + this._config!.show_last_weeks + 1),
+      startDay.getTime() +
+        (this._config.show_last_weeks + this._config.show_follow_weeks + 1) * 7 * 24 * 60 * 60 * 1000 -
+        24 * 60 * 60 * 1000,
     );
-    return element;
 
-    // this.content.innerHTML = `<div class="weekly-calendar">`;
-    // for (
-    //   var currentDay = startDay;
-    //   currentDay <= endDay;
-    //   currentDay.setDate(currentDay.getDate() + 1)
-    // ) {
-    //   isToday = currentDay.getTime() === today.getTime() ? true : false;
-    //   this.content.innerHTML += `
-    //       <div class="day weekday${currentDay.getDay()}">
-    //         <div class="date ${
-    //           isToday ? "today" : ""
-    //         }">${currentDay.getDate()}</div>
-    //       </div>
-    //     `;
-    // }
-    // this.content.innerHTML = `
-    //     </div>
-    //   </div>
-    //   <style>
-    //       .day{
-    //       }
-    //       .date{
-    //       }
-    //       .today{
-    //         background-color:${this.config.todayBackgroundColor}
-    //       }
-    //       `;
-    // for (var weekday = 0; weekday <= 6; weekday++) {
-    //   let style = this.config.weekdayBackgroundColor.find(
-    //     (el) => el.Weekday == weekday
-    //   );
-    //   this.content.innerHTML = `
-    //     .weekday${weekday}{
-    //       background-color:${style.BackgroundColor}
-    //     }`;
-    // }
-    // this.content.innerHTML = `
-    //     </style>
-    //   `;
+    const days: TemplateResult[] = [];
+    for (let currentDay = startDay; currentDay <= endDay; currentDay.setDate(currentDay.getDate() + 1)) {
+      const isToday = currentDay.getTime() === today.getTime() ? true : false;
+      const tomorrow = new Date(currentDay.getDate() + 1);
+      const isStartOfWeek = currentDay.getDay() === this._config.start_weekday ? true : false;
+      const isEndOfWeek = tomorrow.getDay() === this._config.start_weekday ? true : false;
+      days.push(html`
+        ${isStartOfWeek ? '<tr class"week">' : ''}
+        <td class="day weekday${currentDay.getDay()}" ${isToday ? 'today' : ''}>
+          <div>${currentDay.getDate()}</div>
+        </td>
+        ${isEndOfWeek ? '</tr>' : ''}
+      `);
+    }
+    return html`
+      <ha-card
+        ><table calss="calendar">
+          ${days}
+        </table></ha-card
+      >
+    `;
+  }
+
+  protected get styles(): CSSResult | CSSResult[] {
+    const base = css`
+      ha-card {
+        padding: 16px;
+      }
+      .warning {
+        display: block;
+        color: black;
+        background-color: #fce588;
+        padding: 8px;
+      }
+      .calendar {
+      }
+      .week {
+      }
+      .day {
+        text-align: center;
+        vertical-align: middle;
+        padding: 8px;
+      }
+    `;
+    if (!this._config || !this.hass) {
+      return base;
+    }
+
+    const today = css`
+      .today {
+        background-color: ${unsafeCSS(this._config.today_background_color)};
+        color: ${unsafeCSS(this._config.today_text_color)};
+      }
+    `;
+    const weekdays = this._config.weekday_background_color.map(
+      week =>
+        css`
+          .weekday {
+            background-color: ${unsafeCSS(week.background_color)};
+            color: ${unsafeCSS(week.text_color)};
+          }
+        `,
+    );
+
+    return [base, today].concat(weekdays);
   }
 }
 
