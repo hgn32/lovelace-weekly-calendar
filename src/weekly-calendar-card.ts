fc58a7afd640bf2339eae0f7209fb0dc29ba9fd3
hgn32 <yuukies@gmail.com>
2022-04-15 12:27:57 +0900

    Update weekly-calendar-card.ts

--- src/weekly-calendar-card.ts
+++ src/weekly-calendar-card.ts
@@ -101,11 +101,11 @@ class WeeklyCalendarCard extends LitElement {
       if(currentDay.getTime() === today.getTime()) class_list.push("today");
       if(currentDay.getDate() === 1) class_list.push("firstday_of_month");
       if(!class_list.includes("firstday_of_month") && !class_list.includes("firstweek_of_month") && !class_list.includes("lastweek_of_month"))
-          class_list.push("day_base");
+          class_list.push("border_base");
       if(count++ < 7){
         // prettier-ignore
         headers.push(html`
-          <div class="header day_base">
+          <div class="header day_base border_base">
             <div>${weekday_view[currentDay.getDay()]}</div>
           </div>
         `);
@@ -137,12 +137,8 @@ class WeeklyCalendarCard extends LitElement {
         border-bottom: 1px solid ${unsafeCSS(this._config.border_color)};
         border-left:   1px solid ${unsafeCSS(this._config.border_color)};
       }
-      .day_base{
+      .border_base{
         border: 1px solid ${unsafeCSS(this._config.border_color)};
-      	width: calc((100% / 7) - 22px);
-        text-align: center;
-        vertical-align: middle;
-        margin: 0;
       }
       .today {
         background-color: ${unsafeCSS(this._config.today_background_color)};
@@ -189,6 +185,12 @@ class WeeklyCalendarCard extends LitElement {
         flex-wrap: wrap;
         width: 100%;
         justify-content: center;
+      }
+      .day_base{
+      	width: calc((100% / 7) - 22px);
+        text-align: center;
+        vertical-align: middle;
+        margin: 0;
       }
       .header{
         padding: 4px 10px;
